aaaa01454afa42edc332a85d5bea9197dac16c87
Radford Smith <rads@ftml.net>
2009-10-08 22:04:50 -0500

    added stuff

--- rads.el
+++ rads.el
@@ -15,3 +15,32 @@
 ;; Shell
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 (shell)
+
+;; swank-clojure
+(add-to-list 'load-path "~/.emacs.d/vendor/swank-clojure/src/emacs")
+
+(setq swank-clojure-jar-path "/usr/local/clojure/clojure.jar"
+      swank-clojure-extra-classpaths (list
+				      "~/.emacs.d/vendor/swank-clojure/src/main/clojure"
+				      "/usr/local/clojure-contrib/clojure-contrib.jar"))
+
+(require 'swank-clojure-autoload)
+
+;; slime
+(eval-after-load "slime"
+  '(progn (slime-setup '(slime-repl))))
+
+(add-to-list 'load-path "~/.emacs.d/vendor/slime")
+(require 'slime)
+(slime-setup)
+
+;; TextMate
+(add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
+(require 'textmate)
+(textmate-mode)
+
+;; yasnippet
+(add-to-list 'load-path "~/.emacs.d/vendor/yasnippet")
+(require 'yasnippet) ;; not yasnippet-bundle
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets")
