576924b75160435dd22a5bf4cd19721724e830d3
Radford Smith <rads@ftml.net>
2009-10-14 18:15:24 -0500

    added yasnippets-rails, removed flyspell default

--- rads.el
+++ rads.el
@@ -53,6 +53,23 @@
 (require 'yasnippet) ;; not yasnippet-bundle
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets")
+(yas/load-directory "~/.emacs.d/vendor/yasnippets-rails/rails-snippets")
+
+;; ruby
+(add-hook 'ruby-mode-hook 'whitespace-mode)
+
+(dolist (command '(yank yank-pop))
+       (eval `(defadvice ,command (after indent-region activate)
+                (and (not current-prefix-arg)
+                     (member major-mode '(emacs-lisp-mode lisp-mode
+                                                          clojure-mode    scheme-mode
+                                                          haskell-mode    ruby-mode
+                                                          rspec-mode      python-mode
+                                                          c-mode          c++-mode
+                                                          objc-mode       latex-mode
+                                                          plain-tex-mode))
+                     (let ((mark-even-if-inactive transient-mark-mode))
+                       (indent-region (region-beginning) (region-end) nil))))))
 
 (server-start)
 (shell)
