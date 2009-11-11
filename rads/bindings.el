86536f81d69bc087a76450ab14f06dbdd9e02c2e
Radford Smith <github.com@radspam.ftml.net>
2009-11-11 17:51:26 -0600

    start fresh

--- rads/bindings.el
+++
@@ -1,6 +0,0 @@
-(global-set-key "\C-w" 'backward-kill-word)
-(global-set-key "\C-x\C-k" 'kill-region)
-(global-set-key "\C-c\C-k" 'kill-region)
-(global-set-key [C-tab] 'other-window)
-
-(defalias 'qrr 'query-replace-regexp)
