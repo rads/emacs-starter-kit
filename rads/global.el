dcef2379e7cd8f4cad9777abe1abfb61a076d444
Radford Smith <github.com@radspam.ftml.net>
2009-11-11 19:29:17 -0600

    add color-theme

---
+++ rads/global.el
@@ -0,0 +1,11 @@
+;; indentation
+(setq-default tab-width 2)
+(setq-default c-indent-level 2)
+(setq-default indent-tabs-mode nil)
+(prefer-coding-system 'utf-8)
+
+;; font
+(setq default-frame-alist '((font . "-unknown-Consolas-normal-normal-normal-*-15-*-*-*-m-0-*-*")))
+
+(server-start)
+(shell)
